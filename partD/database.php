3b4560cc22da9c9eeefd77726673a201a7efac50
Ubuntu <ubuntu@ip-172-31-12-249.ap-southeast-2.compute.internal>
2013-08-31 13:21:01 +0000

    modify database php

--- partD/database.php
+++ partD/database.php
@@ -2,6 +2,6 @@
 define('DB_ENGINE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'winestore');
-define('DB_USER', 'winestoreuser');
+define('DB_USER', 'webadmin');
 define('DB_PW', '');
 ?>
